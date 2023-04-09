6d0aad71146ebf21cbddd6278dfe957ffee5bbb8
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-09 20:23:17 +0900

    create blog categories details

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -130,6 +130,15 @@ class BlogController extends Controller
         $blogs = Blog::findOrFail($id);
         $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
         return view('frontend.blog_details', compact('blogs', 'allblogs', 'categories'));
+        
+    } // End Method
+    
+    public function CategoryBlog($id) {
+        
+        $blogpost = Blog::where('blog_category_id', $id)->orderBy('id', 'DESC')->get();
+        $allblogs = Blog::latest()->limit(5)->get();
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+        return view('frontend.cat_blog_details', compact('blogpost', 'allblogs', 'categories'));
 
     } // End Method
 
