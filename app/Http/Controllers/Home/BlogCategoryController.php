77412940bb248cee315c3273527a49a162eb5e9c
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-09 14:33:54 +0900

    create update&delete blog category

--- app/Http/Controllers/Home/BlogCategoryController.php
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -46,4 +46,40 @@ class BlogCategoryController extends Controller
             return redirect()->route('all.blog.category')->with($notification);
 
     } // End Method
+
+    public function EditBlogCategory($id) {
+
+        $blogcategory = BlogCategory::findOrFail($id);
+        return view('admin.blog_category.blog_category_edit', compact('blogcategory'));
+
+    }
+
+    public function UpdateBlogCategory(Request $request, $id) {
+
+        BlogCategory::findOrFail($id)->update([
+            
+            'blog_category' => $request->blog_category,
+
+        ]);
+        $notification = array(
+            'message' => 'Blog Category Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.blog.category')->with($notification);
+
+    } // End Method
+
+    public function DeleteBlogCategory($id) {
+
+        BlogCategory::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Blog Category Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // End Method
 }
